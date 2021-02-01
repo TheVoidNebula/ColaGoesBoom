9ccaa120decab33d3c8239a31eed33a2252f2d5f
TheVoidNebula <75329526+TheVoidNebula@users.noreply.github.com>
2021-02-01 11:01:24 +0100

    new feature
    fusetime is also added

--- Config.cs
+++ Config.cs
@@ -10,13 +10,16 @@ namespace ColaGoesBoom
 {
     public class Config : AbstractConfigSection
     {
-        public bool isEnabled = true;
+        public bool IsEnabled = true;
 
         [Description("How many SCP-207 will be needed to explode?")]
-        public int scp207ExplosionAmount = 5;
+        public int Scp207ExplosionAmount = 5;
 
         [Description("How many grenades will be spawned for the explosion?")]
-        public int scp207NadeAmount = 3;
+        public int Scp207NadeAmount = 3;
+
+        [Description("After how many seconds do the grenades explode after drinking the last SCP-207?")]
+        public float Scp207FuseTime = 0.75f;
 
     }
 }
--- Handlers/EventHandlers.cs
+++ Handlers/EventHandlers.cs
@@ -14,18 +14,18 @@ namespace ColaGoesBoom.Handlers
     {
         public EventHandlers()
         {
-            Server.Get.Events.Player.PlayerItemUseEvent += onItemUse;
+            Server.Get.Events.Player.PlayerItemUseEvent += OnItemUse;
         }
 
-        public void onItemUse(Synapse.Api.Events.SynapseEventArguments.PlayerItemInteractEventArgs ev)
+        public void OnItemUse(Synapse.Api.Events.SynapseEventArguments.PlayerItemInteractEventArgs ev)
         {
             if(ev.State == Synapse.Api.Events.SynapseEventArguments.ItemInteractState.Finalizing && ev.CurrentItem.ItemType == ItemType.SCP207)
             {
-                if(ev.Player.PlayerEffectsController.GetEffect<Scp207>().Intensity == Plugin.Config.scp207ExplosionAmount-1)
+                if(ev.Player.PlayerEffectsController.GetEffect<Scp207>().Intensity == Plugin.Config.Scp207ExplosionAmount-1)
                 {
-                    for(int i = 0; i < Plugin.Config.scp207NadeAmount; i++)
+                    for(int i = 0; i < Plugin.Config.Scp207NadeAmount; i++)
                     {
-                        Map.Get.SpawnGrenade(ev.Player.Position, Vector3.zero, 1, Synapse.Api.Enum.GrenadeType.Grenade);
+                        Map.Get.SpawnGrenade(ev.Player.Position, Vector3.zero, Plugin.Config.Scp207FuseTime, Synapse.Api.Enum.GrenadeType.Grenade);
                     }
                 }
             }
